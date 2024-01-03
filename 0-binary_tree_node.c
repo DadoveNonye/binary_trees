6dfc5e0e04035599826498c161a73a5bebc96e72
dadovenonye <chinonyenmecha@yahoo.com>
2024-01-02 19:50:11 -0800

    binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
-	return (new ? (new->n = value, new->parent = parent,
-	new->left = NULL, new->right = NULL, new) : NULL);
+	return (new
+			? (new->n = value, new->parent = parent,
+			   new->left = NULL, new->right = NULL, new)
+			: NULL);
 }
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Insert node as right-child of another in tree.
+ * @parent: Pointer to node to insert the right-child in.
+ * @value: Value to store in the new node.
+ *
+ * Return: If parent is NULL or an error occurs - NULL.
+ *         Otherwise - pointer to the new node.
+ *
+ * Description: If parent has a right-child, new node takes its place and
+ *              the old right-child becomes the right-child of the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
